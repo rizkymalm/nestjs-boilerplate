695e5170b383584ebbedf8bd0b7e9bf906a2c2ab
rizkymalm <rizkymalm@gmail.com>
2026-07-16 13:15:19 +0700

    feat: user service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,50 +1,35 @@
 import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UpdateUserDto } from './dto/updateUser.dto';
+import { UserService } from './user.service';
 
 // /api/user
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
   @Get()
-  getUsers(@Query('name') name: string) {
-    const users = [
-      {
-        id: 1,
-        name: 'John Doe',
-      },
-      {
-        id: 2,
-        name: 'Jane Doe',
-      },
-    ];
-    if (name) {
-      return users.filter((data) =>
-        data.name.toLocaleLowerCase().includes(name.toLowerCase()),
-      );
-    }
-    return users;
+  getUsers(@Query('name') name: string): unknown {
+    return this.userService.findAllUsers(name);
   }
   @Get(':id')
-  getUserById(@Param('id') id: string) {
-    return { id, name: 'John Doe' };
+  getUserById(@Param('id') id: string): unknown {
+    return this.userService.findUserById(parseInt(id));
   }
 
   @Post()
-  createUser(@Body() CreateUserDto: CreateUserDto) {
+  createUser(@Body() CreateUserDto: CreateUserDto): unknown {
+    const data = this.userService.createUser(CreateUserDto);
     return {
-      data: CreateUserDto,
+      data: data,
       message: 'User created successfully!',
     };
   }
 
   @Put(':id')
-  updateUser(@Param('id') id: string, @Body() UpdateUserDto: UpdateUserDto) {
-    return {
-      data: {
-        id,
-        ...UpdateUserDto,
-      },
-      message: 'User updated successfully',
-    };
+  updateUser(
+    @Param('id') id: string,
+    @Body() UpdateUserDto: UpdateUserDto,
+  ): unknown {
+    return this.userService.updateUser(parseInt(id), UpdateUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { LoggerService } from './user.logger';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UpdateUserDto } from './dto/updateUser.dto';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly logger: LoggerService) {}
+  private users: User[] = [
+    {
+      id: 1,
+      name: 'Rizki Malem',
+      email: 'rizkymalm@gmail.com',
+    },
+    {
+      id: 2,
+      name: 'Amajida Zahirah',
+      email: 'amajda@gmail.com',
+    },
+  ];
+  findAllUsers(name: string = '') {
+    this.logger.log('Finding all users');
+    return this.users.filter((data) =>
+      data.name.toLocaleLowerCase().includes(name.toLowerCase()),
+    );
+  }
+
+  findUserById(id: number) {
+    this.logger.log('Get user by id');
+    const user = this.users.find((data) => data.id === id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  createUser(createdUserDto: CreateUserDto) {
+    this.logger.log('Create new user');
+    this.users.push(createdUserDto);
+    return this.users;
+  }
+
+  updateUser(id: number, UpdateUserDto: UpdateUserDto) {
+    const user = this.findUserById(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const update = this.users.map((data) =>
+      data.id === id
+        ? {
+            ...data,
+            name: UpdateUserDto.name,
+            email: UpdateUserDto.email,
+          }
+        : data,
+    );
+    return update;
+  }
+}
